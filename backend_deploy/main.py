3b180755459b29f9d4452b5e0c5bf6b7a8a22621
Ayush Bhatnagar <63344342+ayush-devcore@users.noreply.github.com>
2026-04-16 02:57:43 +0530

    Update main.py

--- backend_deploy/main.py
+++ backend_deploy/main.py
@@ -456,4 +456,4 @@ if __name__ == "__main__":
 |  Running: http://127.0.0.1:8000          |
 +------------------------------------------+
     """)
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+   uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
